8df749bb7a584e33d92e3ca9ebc5e7abd00a57da
justtincho <justincho12345@gmail.com>
2024-07-08 23:56:43 +0800

    Implemented setup for task 3

---
+++ problem3/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WalletPage from './problem3/refactored_code';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WalletPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
